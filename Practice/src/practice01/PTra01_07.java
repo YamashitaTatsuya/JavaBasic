26a06a3346d7549e4673ae00a1983c928af93af9
DESKTOP-J2EIOU0\LIKEIT_STUDENT <tatsuya.yamashita.likeit@gmail.com>
2018-10-25 12:31:24 +0900

    1-7

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,19 +21,28 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
+		bo = true;
+		by = 1;
+		s = 5;
+		c = 'あ' ;
+		i = 100;
+		l = 67678967896L;
+		f = 3.14F;
+		d = 6.2;
 
 
 
 
 		// ★ それぞれの変数の中身を出力してください
 
-
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 
 
