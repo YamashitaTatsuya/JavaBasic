29055cb61876e27627371bb7e1a7a5ce8e0e7a60
DESKTOP-J2EIOU0\LIKEIT_STUDENT <tatsuya.yamashita.likeit@gmail.com>
2018-11-02 13:50:53 +0900

    11-3

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -10,11 +10,22 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-
+		Book[] bookmark = FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
 
+				java.util.Scanner scanner;
+				scanner = new java.util.Scanner(System.in);
+				String line = scanner.nextLine();
+
+		for(Book book: bookmark) {
+			if(book.title.contains(line)) {
+				System.out.println(book.dispBookInfo());
+			}
+		}
+
+		// 本の情報を出力します
 	}
 }
